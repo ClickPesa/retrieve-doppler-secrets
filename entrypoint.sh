45bbf7526526323a7f92a8a8ea7429ef7942a885
Ben <bmsteve96@gmail.com>
2022-05-25 16:29:16 +0300

    test docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-chmod +x entrypoint.sh
-
 echo "$1"
 
-# cd /home/ubuntu/www/v2/master
+cd /home/ubuntu/www/v2/master
 # Configure new environment variables
-# doppler configure set token $1 --scope /home/ubuntu/www/v2/master
+doppler configure set token $1 --scope /home/ubuntu/www/v2/master
 # doppler run -- printenv | grep DOPPLER
 # echo "doppler secrets get DEVTO_API_KEY --plain"
