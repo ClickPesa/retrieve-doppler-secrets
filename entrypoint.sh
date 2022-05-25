95e79a367c6ffd5f6386ad140ea2e39163143a7c
Ben <bmsteve96@gmail.com>
2022-05-25 15:44:57 +0300

    test docker

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "$1"
+
+cd /home/ubuntu/www/v2/develop
+# Perform node_modules package installation
+yarn install
+# Stop current process
+pm2 stop scripts/doppler-run.sh
+# Configure new environment variables
+doppler configure set token $1 --scope /home/ubuntu/www/v2/develop
+# Start new process
+pm2 start scripts/doppler-run.sh --name S1
+# Clean up build archives
+rm -rf $2
