1a4102035815b8871d5c8e1db786ef7f77d05d01
Ben <bmsteve96@gmail.com>
2022-05-30 22:58:40 +0300

    test doppler secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-RUN npm install curl
 (curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sh
 npm run doppler-install && ./doppler run --token $1
 doppler run -- printenv | grep DOPPLER
