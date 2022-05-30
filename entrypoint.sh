2373f4a4040e9fcf16be91dea15d797ed8584ac6
Ben <bmsteve96@gmail.com>
2022-05-30 23:37:14 +0300

    test doppler secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-yarn add -g curl
+yarn
+yarn add curl
+apt-get update
+apt-get -y install curl
 yarn run doppler-install && ./doppler run --token $1
 # (curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sh
-yarn run doppler-install && ./doppler run --token $1
 doppler run -- printenv | grep DOPPLER
 echo "doppler secrets get $REACT_APP_REST_URL --plain"
