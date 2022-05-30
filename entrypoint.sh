e28089003f7125a01866acf028967b79b428eefb
Ben <bmsteve96@gmail.com>
2022-05-30 22:46:30 +0300

    test doppler secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
-!/bin/sh
-
-# RUN (curl -Ls https://cli.doppler.com/install.sh || wget -qO- https://cli.doppler.com/install.sh) | sh -s -- --no-install --no-package-manager
+#!/bin/sh
 RUN (curl -Ls https://cli.doppler.com/install.sh || wget -qO- https://cli.doppler.com/install.sh) | sh -s -- --no-install --no-package-manager
 npm run doppler-install && ./doppler run --token $1
 echo "doppler secrets get $REACT_APP_REST_URL --plain"
