95bc2cce0d8c2a01d8f75c0321e11d24b15bc8ec
Ben <bmsteve96@gmail.com>
2022-05-30 22:24:05 +0300

    test doppler secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-RUN (curl -Ls https://cli.doppler.com/install.sh || wget -qO- https://cli.doppler.com/install.sh) | sh
-
-ls
-
-# cd /home/ubuntu/www/v2/master
-# Configure new environment variables
-# doppler configure set token $1 --scope /home/ubuntu/www/v2/master
-doppler run -- printenv | grep DOPPLER
-echo "doppler secrets get REACT_APP_REST_URL --plain"
+RUN (curl -Ls https://cli.doppler.com/install.sh || wget -qO- https://cli.doppler.com/install.sh) | sh -s -- --no-install --no-package-manager
+# RUN (curl -Ls https://cli.doppler.com/install.sh || wget -qO- https://cli.doppler.com/install.sh) | sh
+npm run doppler-install && ./doppler run --token $DOPPLER_TOKEN_DVP
+# doppler run -- printenv | grep DOPPLER
+echo "doppler secrets get $REACT_APP_REST_URL --plain"
