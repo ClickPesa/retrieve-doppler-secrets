2481908923f34cd84ccaded33e92326d9cb42628
Ben <bmsteve96@gmail.com>
2022-05-30 23:58:37 +0300

    test doppler secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 yarn run doppler-install && ./doppler run --token $1
 doppler run -- printenv | grep DOPPLER
-echo "doppler secrets get $REACT_APP_REST_URL --plain"
+# echo "doppler secrets get $REACT_APP_REST_URL --plain"
