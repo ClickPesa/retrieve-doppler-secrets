cbd8588e551ea4dedc47f1838a66343ba4d709bf
Ben <bmsteve96@gmail.com>
2022-05-31 01:34:25 +0300

    test output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ echo "$1"
 yarn run doppler-install
 # Configure new environment variables
 ./doppler configure set token "$1"
-./doppler run -- printenv | grep DOPPLER
+echo ./doppler run -- printenv | grep DOPPLER >> $GITHUB_ENV
 # echo "::set-output name=env::$(./doppler secrets get --plain)')"
