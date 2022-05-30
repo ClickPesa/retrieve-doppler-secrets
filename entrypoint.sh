b5d5968f7afc5c9caa2da5300aa8efb4f510c7ae
Ben <bmsteve96@gmail.com>
2022-05-31 01:13:44 +0300

    test output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ yarn run doppler-install
 # Configure new environment variables
 ./doppler configure set token "$1"
 ./doppler run -- printenv | grep DOPPLER
-echo "$(./doppler secrets get REACT_APP_REST_URL --plain)"
 echo "$(./doppler run -- printenv | grep DOPPLER)" >> $GITHUB_ENV
+echo "::set-output name=env::$(./doppler run -- printenv | grep DOPPLER)')"
