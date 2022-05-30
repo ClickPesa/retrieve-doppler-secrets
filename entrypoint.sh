6b8bfe53a28220fdd428db929480eb12a84bb753
Ben <bmsteve96@gmail.com>
2022-05-30 22:20:58 +0300

    test doppler secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "$1"
+RUN (curl -Ls https://cli.doppler.com/install.sh || wget -qO- https://cli.doppler.com/install.sh) | sh
 
 ls
 
 # cd /home/ubuntu/www/v2/master
 # Configure new environment variables
 # doppler configure set token $1 --scope /home/ubuntu/www/v2/master
-# doppler run -- printenv | grep DOPPLER
-# echo "doppler secrets get DEVTO_API_KEY --plain"
+doppler run -- printenv | grep DOPPLER
+echo "doppler secrets get REACT_APP_REST_URL --plain"
