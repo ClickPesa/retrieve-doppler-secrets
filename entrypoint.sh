045ef1e1d8693ff72a77acd60d26c2ca2d58c5bc
Ben <bmsteve96@gmail.com>
2022-06-07 09:21:36 +0300

    test splitting variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 echo "$1"
 echo "$2"
+variables=($(echo $2 | tr "," "\n"))
+IFS=', ' read -r -a variables <<< "$string"
+echo "$variables"
+echo "$IFS"
+echo "${variables[0]}"
 yarn run doppler-install
 # Configure new environment variables
 ./doppler configure set token "$1"
