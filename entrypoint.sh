52a75084d7ee47a58a0c9bbfc9be88046cb2f5bc
Ben <bmsteve96@gmail.com>
2022-06-07 09:29:32 +0300

    test splitting variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "$1"
 echo "$2"
-variables=$(echo $2 | tr "," "\n")
+variables=$(echo $2 | tr ", " "\n")
 echo "$variables"
 # echo "${variables[0]}"
 yarn run doppler-install
 # Configure new environment variables
 ./doppler configure set token "$1"
 ./doppler run -- printenv | grep DOPPLER
-echo "$(./doppler secrets get REACT_APP_REST_URL --plain)" >> $GITHUB_ENV
+echo "$(./doppler secrets get REACT_APP_REST_URL)" >> $GITHUB_ENV
 echo "$(./doppler run -- printenv | grep DOPPLER)" >> $GITHUB_ENV
