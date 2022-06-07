3f316f1d159a9de11df4ba43ccfe0445abc5e91d
Ben <bmsteve96@gmail.com>
2022-06-07 09:32:11 +0300

    test splitting variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@ echo "$1"
 echo "$2"
 variables=$(echo $2 | tr ", " "\n")
 echo "$variables"
+## now loop through the above array
+for i in "${variables[@]}"
+do
+   echo "$i"
+done
 # echo "${variables[0]}"
 yarn run doppler-install
 # Configure new environment variables
