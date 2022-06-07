3fe3b95e385d20a754b8d97d99401cf2a67cd1e9
Ben <bmsteve96@gmail.com>
2022-06-07 09:45:30 +0300

    Test loop

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,18 @@ yarn run doppler-install
 ./doppler configure set token "$1"
 ./doppler run -- printenv | grep DOPPLER
 variables=$(echo $2 | tr ", " "\n")
-echo "$variables"
+# echo "$variables"
 ## now loop through the above array
-for i in "${"${variables[@]}"}"; do
-   echo "$i"
-#    echo "$i=$(./doppler secrets get $i --plain)" >> $GITHUB_ENV
+# for i in "${"${variables[@]}"}"; do
+#    echo "$i"
+# #    echo "$i=$(./doppler secrets get $i --plain)" >> $GITHUB_ENV
+# done
+
+# get length of an array
+arraylength=${#variables[@]}
+
+# use for loop to read all values and indexes
+for (( i=0; i<${arraylength}; i++ ));
+do
+  echo "index: $i, value: ${variables[$i]}"
 done
